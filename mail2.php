10d6810b28e0288df6c13dd44d1c36e5556df376
hamza <hamza.bhinder@gmail.com>
2016-04-11 16:10:27 +0500

    Some minor additions + regesteration page

--- mail2.php
+++ mail2.php
@@ -1,73 +1,69 @@
 <?php
+//Personal Info
+$to = "rnfgakcp@mailparser.io";
+$subject = "Azauj registration form";
+$Name=$_POST['name'];
+$Cnic=$_POST['CNIC'];
+$ContactPerson=$_POST['contact'];
+$ContactNumber=$_POST['number'];
+$Email=$_POST['email'];
+$Address=$_POST['address'];
 
 
-	session_start();
+//Credentials
 
-	require_once 'phpmailer/PHPMailerAutoload.php';
-
-
-    $error = [];
-
-	if (isset($_POST['name'], $_POST['email'], $_POST['number'])) {
-		
-		$fields=[
-                
-               'name'=> $_POST['name'],
-               'email'=> $_POST['email'],
-               'number'=> $_POST['number']
-		];
-
-		foreach ($fields as $field => $data) {
-			if (empty($data)) {
-				$error[]='the'. $field . 'field is required';
-
-
-			}
-		}
-
-
-        if (empty($error)) {
-        	
-        	$m = new PHPMailer;
-        	$m ->isSMTP();
-        	$m ->SMTPAuth = true;
-        	$m ->Host ='smtp.gmail.com';
-        	$m ->Username = 'hamza.bhinder@gmail.com';
-        	$m->Password='**********';
-        	$m ->SMTPSecure = 'ssl';
-        	$m ->Port =465;
-
-            $m->isHTML();
-        	$m->Subject='Azauj Contact form';
-            $m->Body = 'Form:' . $field['name'] . ' ' . $field['email'] . ' ' . $field['number'] . '  ';
-            $m->FromName = 'Contact';
-            $m->AddAddress('hamza.bhinder@gmail.com','Hamza Ali Haider');
-
-            if ($m->send()) {
-            	
-            	header('location:index.html');
-            	die();
-            }
-            
-            else {
-
-            	$error[]='sorry sorry sorry';
-            }
+$Gender=$_POST['gender'];
+$Height=$_POST['height'];
+$Appearence=$_POST['appearence'];
+$Age=$_POST['age'];
+$Degree=$_POST['education_degree'];
+$Institution=$_POST['education_institute'];
+$JobTitle=$_POST['job_title'];
+$Income=$_POST['job_salary'];
+$MaritalStatus=$_POST['marital_status'];
+$NoOfChildren=$_POST['marital_status_children'];
+$ReligiousBelief=$_POST['religious_belief'];
+$Caste=$_POST['caste'];
+$Nationality=$_POST['nationality'];
+$City=$_POST['current_city'];
+$Owner=$_POST['owner'];
+$Country=$_POST['current_country'];
+$Relocate= $_POST['willing_to_relocate'];
+$AboutParents=$_POST['parents_details'];
+$AboutSiblings=$_POST['siblings_details'];
+$Additional=$_POST['additional_information'];
 
 
 
 
-        }
-
-
-           
-			}
-
-		else{
-
-			$error[]='something went wrong';
-		}
 
 
 
-		header('location:index.html');
+
+//Requirments
+
+$AgeFrom=$_POST['age_from'];
+$AgeTo=$_POST['age_to'];
+$HeightFrom=$_POST['height_from'];
+$HeightTo=$_POST['height_to'];
+$EducationReq=$_POST['education_req'];
+$ReligiousBeliefReq=$_POST['religious_belief_req'];
+$CasteReq=$_POST['caste_req'];
+$NationalityReq=$_POST['nationality_req'];
+$AppearenceReq=$_POST['appearence_req'];
+$MaritalStatusReq=$_POST['marital_status_req'];
+$NoOfChildrenReq=$_POST['marital_status_children_req'];
+$CityReq=$_POST['current_city_req'];
+$CountryReq=$_POST['current_country_req'];
+$RelocateReq=$_POST['willing_to_relocate_req'];
+
+
+$txt = "Name: ".$Name.  " \nCnic:" .$Cnic.  "\nContact Person:" .$ContactPerson.  "\nContact Number: " .$ContactNumber. "\nEmail: ".$Email.  "\nAddress:" .$Address.  "\nGender:" .$Gender.  "\nHeight:" .$Height.  "\nBeared/Hijabi:" .$Appearence. "\nAge:"
+    .$Age. "\nDegree:" .$Degree. "\nInstitution:" .$Institution.  "\nJob Title:" .$JobTitle. "\nIncome:" .$Income. "\nMarital Status:" .$MaritalStatus. "\nNumber Of Children:" .$NoOfChildren. "\nReligios belief:" .$ReligiousBelief. "\nCaste:" .$Caste. "\nNationality:" .$Nationality. "\nCity:" .$City. "\nCountry:" .$Country.  "\n About Parents:" .$AboutParents. 
+    "\n About Siblings:" .$AboutSiblings. "\n Additional Information:" .$Additional. "\nWilling To Relocate:" .$Relocate. "\nAge From:" .$AgeFrom. "\nAge To:" .$AgeTo. 
+    "\nHeight From:" .$HeightFrom. "\nHeight To:" .$HeightTo. "\nEducation:" .$EducationReq. "\nReligious Belief:" .$ReligiousBeliefReq. "\nCaste:" .$CasteReq. "\nNationality:" .$NationalityReq. "\nAppearence:" .$AppearenceReq. 
+    "\nMarital Status:" .$MaritalStatusReq. "\nNumber Of children:" .$NoOfChildrenReq. "\nCity:" .$CityReq. "\nCountry:" .$CountryReq. "\nRelocate:" .$RelocateReq; 
+$headers = $Email ;
+
+mail($to, $subject, $txt, $headers);
+header('location:index.html');
--- mail3.php
+++ mail3.php
@@ -1,4 +1,5 @@
 <?php
+
 $to = "hamza.bhinder@gmail.com";
 $subject = "Azauj Enquiries";
 $Name=$_POST['name'];
@@ -8,6 +9,5 @@ $txt = "Name: ".$Name. "Number: " .$Number. "Email: ".$Email;
 $headers = $Email. "\r\n" .
 "CC: somebodyelse@example.com";
 
-mail($to,$subject,$txt,$headers);
+mail($to, $subject, $txt, $headers);
 header('location:index.html');
-?>
