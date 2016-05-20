ca570e2dd6dbce06f3e96c70004655fd08c3fa95
hamza <hamza.bhinder@gmail.com>
2016-05-21 01:02:40 +0500

    img css index.html changes

--- mail3.php
+++ mail3.php
@@ -1,13 +1,14 @@
 <?php
-
-$to = "hamza.bhinder@gmail.com";
+$to = "pxkwsfzc@mailparser.io";
 $subject = "Azauj Enquiries";
 $Name=$_POST['name'];
 $Number=$_POST['number'];
 $Email=$_POST['email'];
-$txt = "Name: ".$Name. "Number: " .$Number. "Email: ".$Email;
+$txt = "Name: ".$Name.  "\nNumber: " .$Number. "\nEmail: ".$Email;
 $headers = $Email. "\r\n" .
-"CC: somebodyelse@example.com";
+"CC: zitavincenza@gmail.com";
 
-mail($to, $subject, $txt, $headers);
+mail($to,$subject,$txt,$headers);
 header('location:index.html');
+exit();
+?>
